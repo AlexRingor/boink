d00f9b0823c0a29d7803f4ac558c794b0216e9d3
Alex Ringor <alexander.ringor@conveyancing.com.au>
2022-07-08 10:38:59 +0800

    add suspense to Components to delay component load when data fetching is still inprogress

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import './App.css';
-import Recommended from './Recommended'
-import Notifications from './Notifications'
-import Suspense from 'react'
+// import Recommended from './Recommended'
+// import Notifications from './Notifications'
+import React, {Suspense} from 'react'
+
+const Recommended = React.lazy(() => {
+  return import('./Recommended')
+})
+
+const Notifications = React.lazy(() => {
+  return import('./Notifications')
+})
+
 
 function App() {
   return (
     <div className="App">
       <h1>SW Coding Challenge</h1>
-      <Recommended />
+      <Suspense fallback={<h1>You are seeing this because 'Internet' decide to take is slow</h1>} >
+        <Recommended />
+        <Notifications />
+      </Suspense>
       <br></br>
-      <Notifications />
+      
     </div>
   );
 }
